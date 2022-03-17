a074a00b751c66a1e4d9c2466e3c1f426a3903a9
Fabien Ducret <>
2022-03-17 16:09:34 +0100

    feat: cast to str

--- src/presenters/console_presenter.rs
+++ src/presenters/console_presenter.rs
@@ -9,15 +9,15 @@ impl Presenter for ConsolePresenter {
     }
 
     fn print_text_blue(&mut self, text: &str) {
-        self.print_text(format!("\x1b[34m{}\x1b[0m", text).as_str());
+        self.print_text(&format!("\x1b[34m{}\x1b[0m", text)[..]);
     }
 
     fn print_text_green(&mut self, text: &str) {
-        self.print_text(format!("\x1b[32m{}\x1b[0m", text).as_str());
+        self.print_text(&format!("\x1b[32m{}\x1b[0m", text)[..]);
     }
 
     fn print_text_red(&mut self, text: &str) {
-        self.print_text(format!("\x1b[31m{}\x1b[0m", text).as_str());
+        self.print_text(&format!("\x1b[31m{}\x1b[0m", text)[..]);
     }
 
     fn ask_for_line(&mut self, question: &str) -> String {
